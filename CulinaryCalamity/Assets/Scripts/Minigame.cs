2e8c1c5ee98826d7014e603a98f55cc2a398dbdf
jbursch <jtb9942@uncw.edu>
2024-02-18 10:15:56 -0500

    Created MiniGame, Recipe, and Ingredient data models

--- CulinaryCalamity/Assets/Scripts/Minigame.cs
+++ CulinaryCalamity/Assets/Scripts/Minigame.cs
@@ -3,5 +3,10 @@ using UnityEngine;
 
 public class Minigame : MonoBehaviour
 {
+    private Recipe recipe;
+    private AudioClip audioFile;
+    private float timingInformation;
+    private int score;
+    private float rewardMultiplier;
 
 }
--- CulinaryCalamity/Assets/Scripts/Recipe.cs
+++ CulinaryCalamity/Assets/Scripts/Recipe.cs
@@ -1,7 +1,11 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 
 public class Recipe : MonoBehaviour
 {
-
+    private string recipeName;
+    private List<Ingredient> ingredients;
+    private Sprite ingSprite;
+    private int rewardAmount;
 }
