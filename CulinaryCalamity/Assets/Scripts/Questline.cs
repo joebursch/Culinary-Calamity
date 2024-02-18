eba213673814a2e7f840bca525d2a90bb18eb49e
bradleyhays <126297277+bradleyhays@users.noreply.github.com>
2024-02-18 16:40:11 -0500

    Initial questline declarations

--- CulinaryCalamity/Assets/Scripts/Questline.cs
+++ CulinaryCalamity/Assets/Scripts/Questline.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 
 public class Questline : MonoBehaviour
 {
-
+    private Dictionary<int, object[]> quests; // <int questID, [*quest stats*]>; // Quests within the questline 
+    private Item[] rewards; // Rewards for completing the questline.
+    private bool completed; // Is the questline finished?
 }
