eaaf493f3fa7041e6f2eb59d5219bf09e71b958e
bradleyhays <126297277+bradleyhays@users.noreply.github.com>
2024-02-18 16:52:09 -0500

    Initial savefile declarations

--- CulinaryCalamity/Assets/Scripts/Savefile.cs
+++ CulinaryCalamity/Assets/Scripts/Savefile.cs
@@ -1,7 +1,14 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 
-public static class Savefile
+public class Savefile : MonoBehaviour
 {
-
+    private int playerHealth; // The player's health
+    private Item[] inventory; // The player's inventory
+    private UnityEngine.SceneManagement.Scene currentScene; // Current scene player is in
+    private Dictionary<int, object[]> characterStats; // <int characterUID, [int characterID, int characterHealth, int characterTimesKilled, Vector3 characterLocation, Item[] characterInventory]>
+    private Dictionary<int, object[]> quests; // <int questID, [*quest stats*]>
+    private Dictionary<int, object[]> minigame; // <int minigameID, [*minigame stats*]>
+    private Dictionary<int, object[]> forageables; // <int objectUID, [*object stats*]>
 }
