1a260d231131f231a708982f112ad93ddff8653b
Andrew Lawson <acl9817@uncw.edu>
2024-02-19 12:43:52 -0500

    Finish data models and test movement

--- CulinaryCalamity/Assets/Scripts/Character.cs
+++ CulinaryCalamity/Assets/Scripts/Character.cs
@@ -6,41 +6,11 @@ public class Character : MonoBehaviour
 {
     // Private instance variables
 
-    [SerializeField]
-    private string characterName;
     
-    [SerializeField]
-    private Sprite mainSprite;
-    [SerializeField]
-    protected float movementSpeed;
-    [SerializeField]
-    protected int characterHealth;
-    [SerializeField]
-    private List<Animation> characterAnimations;
-    [SerializeField]
-    private Animator characterAnimator;
-
-    public Character()
-    {
-
-    }
-    // Constructors
-    public Character(string name, float speed, int health)
-    {
-        characterName = name;
-        movementSpeed = speed;
-        characterHealth = health;
-    }
-    public Character(string name, Sprite sprite, float speed, int health, List<Animation> animations)
-    {
-        characterName = name;
-        mainSprite = sprite;
-        movementSpeed = speed;
-        characterHealth = health;
-        characterAnimations = animations;
-    }
-
-    public string getName() { return characterName; }
+    [SerializeField] protected string characterName;
+    [SerializeField] protected float movementSpeed;
+    [SerializeField] protected int characterHealth;
+    [SerializeField] protected Animator characterAnimator;
 }
 
 
--- CulinaryCalamity/Assets/Scripts/Player.cs
+++ CulinaryCalamity/Assets/Scripts/Player.cs
@@ -6,15 +6,11 @@ using UnityEngine.InputSystem;
 [System.Serializable]
 public class Player : Character
 {
+   [SerializeField] private List<Item> playerInventory; // Update to Inventory reference
+   [SerializeField] private int amountOfGold;
+   private Questline questline;
    private int currentHealth; 
-   [SerializeField]
-   private List<Item> playerInventory; 
-   [SerializeField]
-   private int amountOfGold;
-   private Animation attackAnimation;
-   private Animation cookingAnimation;
    private Actions controlScheme = null;
-
    private Vector2 movementDir;
 
    void Awake() => controlScheme = new Actions();
@@ -23,29 +19,13 @@ public class Player : Character
 
    void OnDestroy() => controlScheme.Standard.Disable();
 
-   // Constructors
-   public Player(string name, float speed, int health, List<Item> inventory, int gold) : base(name, speed, health)
-   {
-        playerInventory = inventory;
-        amountOfGold = gold; 
-   }
-
-   public Player(string name, Sprite sprite, float speed, int health, List<Animation> animations, List<Item> inventory, int gold, Animation attack, Animation cook) 
-    : base(name, sprite, speed, health, animations)
-   {
-        playerInventory = inventory;
-        amountOfGold = gold; 
-        attackAnimation = attack;
-        cookingAnimation = cook;
-   }
-
    void movePlayer()
    {
 
      movementDir = controlScheme.Standard.Move.ReadValue<Vector2>();
      if(movementDir.x > 0 | movementDir.x < 0){movementDir.y = 0;}
      transform.Translate(movementDir * movementSpeed * Time.deltaTime);
-     
+
    }
 
    // Player movement 
