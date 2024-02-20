19fa1244845cbdfae454c7c4ef337f560d6d198f
Brennon Treadwell <bjt1798@uncw.edu>
2024-02-20 11:30:49 -0500

    update item data model

--- CulinaryCalamity/Assets/Scripts/Item.cs
+++ CulinaryCalamity/Assets/Scripts/Item.cs
@@ -5,6 +5,7 @@ public class Item : MonoBehaviour
 {
     // functions like a primary key
     private int itemId;
+    private string itemName;
     [SerializeField] private Sprite itemSprite;
     private int sellPrice;
     private int buyPrice;
