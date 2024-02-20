539f61039848c22374dae82376819385a5f6a66e
bradleyhays <126297277+bradleyhays@users.noreply.github.com>
2024-02-19 22:09:31 -0500

    Remove a line and fix comments

--- CulinaryCalamity/Assets/Scripts/Savefile.cs
+++ CulinaryCalamity/Assets/Scripts/Savefile.cs
@@ -9,7 +9,6 @@ public class Savefile : MonoBehaviour
     private UnityEngine.SceneManagement.Scene currentScene; // Current scene player is in
     private List<Character> allCharacterStats; // Stats of every character
     private List<Quest> questStats; // Quest stats
-    private List<Minigame> minigameStats; // <int minigameID, [*minigame stats*]>
-    private List<int, object[]> forageables; // <int objectUID, [*object stats*]>
+    private List<Minigame> minigameStats; // Minigame stats
     private List<Item> items; // Items not in any inventory
 }
