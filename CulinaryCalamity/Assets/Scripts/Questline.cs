7772170c2d2b5d31ecdae381052b6a052a7d9352
bradleyhays <126297277+bradleyhays@users.noreply.github.com>
2024-02-20 13:17:56 -0500

    Definition and comment changes

--- CulinaryCalamity/Assets/Scripts/Questline.cs
+++ CulinaryCalamity/Assets/Scripts/Questline.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Questline : MonoBehaviour
 {
-    private List<Quest> quests; // Quests within the questline 
-    private List<Item> rewards; // Rewards for completing the questline.
-    private bool questlineCompleted; // Is the questline finished?
+    private List<Quest> questlineQuests;
+    private List<Item> questlineRewards;
+    private bool questlineCompleted;
 }
