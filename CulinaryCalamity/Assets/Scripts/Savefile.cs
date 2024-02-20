24d199815e98e5971adb082c5a9ad7ab55f2fa15
bradleyhays <126297277+bradleyhays@users.noreply.github.com>
2024-02-20 13:16:09 -0500

    More definition and comment changes for clarification

--- CulinaryCalamity/Assets/Scripts/Savefile.cs
+++ CulinaryCalamity/Assets/Scripts/Savefile.cs
@@ -6,7 +6,7 @@ public class Savefile : MonoBehaviour
 {
     private Player player; // To save player's health, inventory, and other information
     private List<Character> allCharacterStats; // Get the information from every character in the game to trace position/status
-    private List<Quest> questStats;
-    private List<Minigame> minigameStats;
-    private List<Item> items; // Items not in any inventory (on ground, etc.)
+    private List<Quest> playerQuestStats;
+    private List<Minigame> playerMinigameStats;
+    private List<Item> unheldItems; // Items not in any inventory (on ground, etc.)
 }
