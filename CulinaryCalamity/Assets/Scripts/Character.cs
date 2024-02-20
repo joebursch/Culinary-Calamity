0c18c488ab1e285a4f3964907706067175d8bc65
Brennon Treadwell <bjt1798@uncw.edu>
2024-02-20 13:25:37 -0500

    change Character

--- CulinaryCalamity/Assets/Scripts/Character.cs
+++ CulinaryCalamity/Assets/Scripts/Character.cs
@@ -5,43 +5,11 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
     // Private instance variables
-    private string characterName;
-    [SerializeField]
-    private Sprite mainSprite;
-    private float movementSpeed;
-    private int characterHealth;
-    [SerializeField]
-    private List<Animation> characterAnimations;
-
-    public Character()
-    {
-
-    }
-    // Constructors
-    public Character(string name, float speed, int health)
-    {
-        characterName = name;
-        movementSpeed = speed;
-        characterHealth = health;
-    }
-    public Character(string name, Sprite sprite, float speed, int health, List<Animation> animations)
-    {
-        characterName = name;
-        mainSprite = sprite;
-        movementSpeed = speed;
-        characterHealth = health;
-        characterAnimations = animations;
-    }
-
-    public Character(string name, Sprite sprite, float speed, List<Animation> animations) 
-    {
-        this.characterName = name;
-        this.mainSprite = sprite;
-        this.movementSpeed = speed;
-        this.characterAnimations = animations;
-    }
-
-    public string getName() { return characterName; }
+    [SerializeField] protected string characterName;
+    [SerializeField] protected float movementSpeed;
+    [SerializeField] protected int characterHealth;
+    [SerializeField] protected Animator characterAnimator;
+    protected int currentHealth;
 }
 
 
