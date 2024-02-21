0951db90dc9c46e0fa897366d0c2e75f8aa8e60d
bradleyhays <126297277+bradleyhays@users.noreply.github.com>
2024-02-20 20:24:28 -0500

    Initial quest declarations

--- CulinaryCalamity/Assets/Scripts/Quest.cs
+++ CulinaryCalamity/Assets/Scripts/Quest.cs
@@ -1,7 +1,9 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 
 public class Quest : MonoBehaviour
 {
-
+    private int questID;
+    private Dictionary<string, object> questAttributes; // <"name", "Quest Name">, <"reward", List<Item> questReward>, etc.
 }
