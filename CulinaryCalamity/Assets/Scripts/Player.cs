7857a960eeaf8268c7c5d1f73eab02b09d7b778e
Brennon Treadwell <bjt1798@uncw.edu>
2024-02-26 17:44:06 -0500

    ready for initial testing

--- CulinaryCalamity/Assets/Scripts/Player.cs
+++ CulinaryCalamity/Assets/Scripts/Player.cs
@@ -1,53 +1,76 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using Saving;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 [System.Serializable]
 public class Player : Character
 {
-   [SerializeField] private List<Item> playerInventory; // Update to Inventory reference
-   [SerializeField] private int amountOfGold;
-   private Questline questline;
-   private Actions controlScheme = null;
-   private Vector2 movementDir;
-   [SerializeField] private LayerMask solidObjectsLayer;
+    [SerializeField] private List<Item> playerInventory; // Update to Inventory reference
+    [SerializeField] private int amountOfGold;
+    private Questline questline;
+    private Actions controlScheme = null;
+    private Vector2 movementDir;
+    [SerializeField] private LayerMask solidObjectsLayer;
+    private ObjectSaveData playerSaveData;
 
-   void Awake() => controlScheme = new Actions();
+    void Awake()
+    {
+        controlScheme = new Actions();
+        GameSaveManager.GetGameSaveManager().Save += OnSave;
+        GameSaveManager.GetGameSaveManager().Load += OnLoad;
+    }
 
-   void OnEnable() => controlScheme.Standard.Enable();
+    void OnEnable() => controlScheme.Standard.Enable();
 
-   void OnDestroy() => controlScheme.Standard.Disable();
+    void OnDestroy() => controlScheme.Standard.Disable();
 
-   void movePlayer()
-   {
-     // Get vector values for movement
-     movementDir = controlScheme.Standard.Move.ReadValue<Vector2>();
-     // Limit movement to up/down/left/right (No diagonal movement)
-     if(movementDir.x > 0 | movementDir.x < 0){movementDir.y = 0;}
-     // Check if the target position is walkable
-     var targetPos = transform.position;
-     targetPos.x += movementDir.x;
-     targetPos.y += movementDir.y;
-     if(IsWalkable(targetPos))
-     {
-      transform.Translate(movementDir * movementSpeed * Time.deltaTime);
-     }
-   }
+    public void OnSave(object sender, EventArgs e)
+    {
+        Dictionary<string, string> playerData = new()
+        {
+            { "PlayerName", characterName }
+        };
 
-   private bool IsWalkable(Vector3 targetPos)
-   {
-      if(Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectsLayer) != null)
-      {
-        return false;
-      }
+        playerSaveData.UpdateSaveData(playerData);
+        GameSaveManager.GetGameSaveManager().UpdateObjectSaveData("PlayerObject", playerSaveData);
+    }
 
-      return true;
-   }
+    public void OnLoad(object sender, EventArgs e)
+    {
+        playerSaveData = GameSaveManager.GetGameSaveManager().GetObjectSaveData("PlayerObject");
+        characterName = playerSaveData.SaveData["PlayerName"];
+    }
 
-   // Player movement 
-   void Update() 
-   {
-     movePlayer();
-   }
+    void MovePlayer()
+    {
+        // Get vector values for movement
+        movementDir = controlScheme.Standard.Move.ReadValue<Vector2>();
+        // Limit movement to up/down/left/right (No diagonal movement)
+        if (movementDir.x > 0 | movementDir.x < 0) { movementDir.y = 0; }
+        // Check if the target position is walkable
+        var targetPos = transform.position;
+        targetPos.x += movementDir.x;
+        targetPos.y += movementDir.y;
+        if (IsWalkable(targetPos))
+        {
+            transform.Translate(movementDir * movementSpeed * Time.deltaTime);
+        }
+    }
+
+    private bool IsWalkable(Vector3 targetPos)
+    {
+        if (Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectsLayer) != null)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    // Player movement 
+    void Update()
+    {
+        MovePlayer();
+    }
 }
