6a23bd044c063996a1ae03a6d5238967f44057af
Andrew Lawson <acl9817@uncw.edu>
2024-02-27 11:06:35 -0500

    Initial implementation of #18

--- CulinaryCalamity/Assets/Scripts/Player.cs
+++ CulinaryCalamity/Assets/Scripts/Player.cs
@@ -11,6 +11,7 @@ public class Player : Character
     private Questline _questline;
     private Actions _controlScheme = null;
     private Vector2 _movementDir;
+    private float _Running;
     [SerializeField] private LayerMask _solidObjectsLayer;
 
     void Awake()
@@ -27,15 +28,31 @@ public class Player : Character
     {
         // Get vector values for movement
         _movementDir = _controlScheme.Standard.Move.ReadValue<Vector2>();
+        _Running = _controlScheme.Standard.Run.ReadValue<float>();
         // Limit movement to up/down/left/right (No diagonal movement)
         if (_movementDir.x > 0 | _movementDir.x < 0) { _movementDir.y = 0; }
         if (_movementDir != Vector2.zero)
         {
             characterAnimator.SetFloat("moveX", _movementDir.x);
             characterAnimator.SetFloat("moveY", _movementDir.y);
-            characterAnimator.SetBool("isWalking", true);
+
+            if (_Running == 1)
+            {
+                movementSpeed = 10;
+                characterAnimator.SetBool("isRunning", true);
+            }
+            else
+            {
+                movementSpeed = 3;
+                characterAnimator.SetBool("isRunning", false);
+                characterAnimator.SetBool("isWalking", true);
+            }
         }
-        else { characterAnimator.SetBool("isWalking", false); }
+        else
+        {
+            characterAnimator.SetBool("isWalking", false);
+            characterAnimator.SetBool("isRunning", false);
+        }
         // Check if the target position is walkable
         if (IsWalkable())
         {
