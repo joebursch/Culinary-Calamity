f5bbe5f93bf649737e34471a9894124d6ebf3c1b
Andrew Lawson <acl9817@uncw.edu>
2024-03-05 13:18:36 -0500

    item spawning for #29

--- CulinaryCalamity/Assets/Scripts/Forageable.cs
+++ CulinaryCalamity/Assets/Scripts/Forageable.cs
@@ -13,11 +13,29 @@ public class Forageable : MonoBehaviour, InteractableObject
     // 1 or more items dropped when the forageable is harvested
     [SerializeField] private List<GameObject> _itemsDropped;
 
+    private Vector3 _randomSpawnPosition;
 
+    void Awake()
+    {
+        _randomSpawnPosition = Vector3.zero;
+    }
     public void Interact()
     {
-        Debug.Log("Forageable --> Implement Later");
+        SpawnItems();
     }
 
+    private void SpawnItems()
+    {
+        foreach (GameObject gameObject in _itemsDropped)
+        {
+            SetRandomSpawnPosition();
+            Instantiate(gameObject, transform.position + _randomSpawnPosition, Quaternion.identity);
+        }
+    }
 
+    private void SetRandomSpawnPosition()
+    {
+        _randomSpawnPosition.x = Random.Range(-4.0f, 4.0f);
+        _randomSpawnPosition.y = Random.Range(-2.0f, 2.0f);
+    }
 }
