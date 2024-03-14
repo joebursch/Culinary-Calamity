b4f0150b7c38f268afbcd8b793241b4573d04d8e
bradleyhays <126297277+bradleyhays@users.noreply.github.com>
2024-03-13 20:16:56 -0400

    Correction on doors and prefab (#4)

--- CulinaryCalamity/Assets/Scripts/Door.cs
+++ CulinaryCalamity/Assets/Scripts/Door.cs
@@ -3,73 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class Door : MonoBehaviour, InteractableObject
 {
-    private Vector3 doorLocation;
-    private Vector3 doorOthersideLocation;
-    private bool unlocked = true;
+    [SerializeField] private Vector3 doorOthersideLocation;
+    [SerializeField] private bool unlocked = true;
     private bool playerInside = false;
     private string entryScene = "";
 
     // Start is called before the first frame update
     void Start()
     {
-        doorLocation = transform.position;
-        doorOthersideLocation = transform.position;
-        // doorOthersideLocation = Other end TBD
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (entryScene.Equals(""))
-        {
-            if (PlayerAtDoor())
-            {
-                if (Unlocked())
-                {
-                    Open();
-                }
-                else
-                {
-                    // TODO Door is locked.
-                }
-            }
-        }
-        else
-        {
-            if (PlayerAtDoor())
-            {
-                if (Unlocked())
-                {
-                    if (PlayerInside())
-                    {
-                        SceneManager.UnloadSceneAsync("Home");
-                        Open();
-                    }
-                    else
-                    {
-                        SceneManager.LoadSceneAsync("Home");
-                        Open();
-                    }
-                }
-                else
-                {
-                    // TODO Door is locked.
-                }
-            }
-        }
-        if (PlayerAtDoor() & entryScene.Equals(""))
-        {
-            Open();
-            GetComponent<Player>().transform.position = doorOthersideLocation;
-            SceneManager.UnloadSceneAsync("Home");
-        }
-        else if(PlayerAtDoor() & !playerInside)
-        {
-            GetComponent<Player>().transform.position = doorOthersideLocation;
-            SceneManager.LoadSceneAsync("Home");
-        }
+       
     }
 
     void PickUp()
@@ -77,9 +28,12 @@ public class Door : MonoBehaviour, InteractableObject
         // TODO Doors cannot be picked up
     }
 
-    bool PlayerAtDoor()
+    void OnCollisionEnter(Collision collision)
     {
-        return Vector3.Distance(GetComponent<Player>().transform.position, doorLocation) <= 5;
+        if(collision.collider == GetComponent<Player>())
+        {
+            Use();
+        }
     }
 
     bool PlayerInside()
@@ -94,11 +48,36 @@ public class Door : MonoBehaviour, InteractableObject
 
     void Use()
     {
-        if (Unlocked())
+        if (entryScene.Equals(""))
+        { 
+           if (Unlocked())
+                {
+                    Open();
+                    playerInside = true;
+                }
+                else
+                {
+                    // TODO Door is locked.
+                }
+        }
+        else
         {
-            if (PlayerAtDoor())
+            if (Unlocked())
             {
-                Open();
+                if (PlayerInside())
+                {
+                    SceneManager.UnloadSceneAsync("Home");
+                    Open();
+                }
+                else
+                {
+                    SceneManager.LoadSceneAsync("Home");
+                    Open();
+                }
+            }
+            else
+            {
+                // TODO Door is locked.
             }
         }
     }
