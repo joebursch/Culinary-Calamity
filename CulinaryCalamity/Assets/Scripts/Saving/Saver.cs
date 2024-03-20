8e620431fd9e168788be42a903a7745fa583dbdd
Brennon Treadwell <bjt1798@uncw.edu>
2024-03-20 18:27:15 -0400

    remove unused imports,

--- CulinaryCalamity/Assets/Scripts/Saving/Saver.cs
+++ CulinaryCalamity/Assets/Scripts/Saving/Saver.cs
@@ -1,10 +1,7 @@
-using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using UnityEngine;
 using Newtonsoft.Json;
-using Unity.Loading;
 
 namespace Saving
 {
--- CulinaryCalamity/Assets/Scripts/Start Menu/SaveSelectionMenu.cs
+++ CulinaryCalamity/Assets/Scripts/Start Menu/SaveSelectionMenu.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Saving;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
