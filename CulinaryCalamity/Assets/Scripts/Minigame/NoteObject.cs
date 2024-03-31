533e28e9f50b7d6f41aad831077f057e37bcf5a2
jbursch <jtb9942@uncw.edu>
2024-03-30 20:12:34 -0400

    Added visuals for hit and missed notes. Notes are now destroyed when out of view

--- CulinaryCalamity/Assets/Scripts/Minigame/NoteObject.cs
+++ CulinaryCalamity/Assets/Scripts/Minigame/NoteObject.cs
@@ -5,24 +5,23 @@ using UnityEngine;
 public class NoteObject : MonoBehaviour
 {
     public bool canBePressed;
-
     public bool obtained = false;
-
     public KeyCode keyToPress;
+    private GameObject activator; // Reference to the activator object
+    private Color originalColor; // Original color of the activator object
+    private Vector3 originalCameraPosition; // Original position of the camera
+    private Transform cameraTransform; // Reference to the camera transform
+    private float shakeDuration = 0.2f; // Duration of the shake animation
+    private float shakeMagnitude = 0.1f; // Magnitude of the shake
 
-    /// <summary>
-    /// Start is called before the first frame update. 
-    /// It is used for initialization purposes and does not contain any specific functionality in this context.
-    /// </summary>
     void Start()
     {
-        
+        activator = GameObject.FindGameObjectWithTag("Activator"); // Find the activator object by tag
+        originalColor = activator.GetComponent<SpriteRenderer>().color; // Store the original color
+        cameraTransform = Camera.main.transform; // Get the main camera transform
+        originalCameraPosition = cameraTransform.position; // Store the original position of the camera
     }
 
-    /// <summary>
-    /// Update is called once per frame. Handles input for pressing the note and checks if it can be pressed.
-    /// If the note can be pressed and the corresponding key is pressed, triggers the NoteHit function, sets the note as obtained, and deactivates the game object.
-    /// </summary>
     void Update()
     {
         if (Input.GetKeyDown(keyToPress))
@@ -31,36 +30,72 @@ public class NoteObject : MonoBehaviour
             {
                 MiniGameManager.instance.NoteHit();
                 obtained = true;
-                gameObject.SetActive(false);
+                ChangeActivatorColor(Color.yellow); // Change activator color to yellow when hit
+                Invoke("RevertActivatorColor", 0.1f); // Revert activator color back to original after a short delay
+                gameObject.SetActive(false); // Deactivate the note object
             }
+        }
+
+        // Calculate the leftmost and rightmost positions of the note's sprite
+        float leftEdge = transform.position.x - GetComponent<SpriteRenderer>().bounds.extents.x;
+        float rightEdge = transform.position.x + GetComponent<SpriteRenderer>().bounds.extents.x;
+
+        // Check if the note is obtained, and both its left and right edges are to the left of the camera's view
+        if (!obtained && leftEdge < Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).x && rightEdge < Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).x)
+        {
+            Destroy(gameObject); // Destroy the note if it's obtained and completely out of the camera's view on the left side
         }
     }
 
-    /// <summary>
-    /// Called when another collider enters the trigger collider attached to this GameObject.
-    /// </summary>
-    /// <param name="other">The other collider.</param>
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.CompareTag("Activator"))
+        if (other.CompareTag("Activator"))
         {
             canBePressed = true;
         }
     }
 
-    /// <summary>
-    /// Called when another collider exits the trigger collider attached to this GameObject.
-    /// </summary>
-    /// <param name="other">The other collider.</param>
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Activator"))
         {
             canBePressed = false;
-            if(!obtained)
+            if (!obtained)
             {
                 MiniGameManager.instance.NoteMissed();
+                ChangeActivatorColor(Color.red);
+                Invoke("RevertActivatorColor", 0.1f);
+                StartCoroutine(ShakeScene()); // Trigger scene shaking animation when missed
             }
         }
+    }
+
+    void ChangeActivatorColor(Color color)
+    {
+        activator.GetComponent<SpriteRenderer>().color = color; // Change activator color
+    }
+
+    void RevertActivatorColor()
+    {
+        activator.GetComponent<SpriteRenderer>().color = originalColor; // Revert activator color back to original
+    }
+
+    IEnumerator ShakeScene()
+    {
+        float elapsed = 0.0f;
+
+        while (elapsed < shakeDuration)
+        {
+            float x = originalCameraPosition.x + Random.Range(-shakeMagnitude, shakeMagnitude);
+            float y = originalCameraPosition.y + Random.Range(-shakeMagnitude, shakeMagnitude);
+
+            cameraTransform.position = new Vector3(x, y, originalCameraPosition.z);
+
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        cameraTransform.position = originalCameraPosition;
     }
 }
